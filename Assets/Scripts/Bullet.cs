075ccdf04de80cceb9be214f3eb23fb2f11ec9cf
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2019-04-10 12:01:42 +0100

    Added bullets and enemy shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int bulletspeed;
+    void Start()
+    {
+        this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * bulletspeed);
+    }
+}
